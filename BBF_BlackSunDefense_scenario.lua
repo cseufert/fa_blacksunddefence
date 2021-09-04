6d832dc544f283ca4407d97a1443469325be2869
Chris Seufert <chris@modd.com.au>
2021-09-04 19:48:35 +1000

    update description

--- BBF_BlackSunDefense_scenario.lua
+++ BBF_BlackSunDefense_scenario.lua
@@ -1,33 +1,39 @@
-version = 3
+version = 3 -- Lua Version. Dont touch this
 ScenarioInfo = {
-  Configurations={
-    standard={
-      customprops={ ExtraArmies="ARMY_HQ ARMY_INCOMING_ENEMY" },
-      teams={
-        { 
-          armies={ "ARMY_1", "ARMY_2", "ARMY_3", "ARMY_4" }, 
-          name="FFA" 
-		}
-      }
-    }
-  },
-  description="Check http://brunobf.blogspot.com/p/black-sun-defense.html for more information.",
-  map="/maps/BBF_BlackSunDefense/BBF_BlackSunDefense.scmap",
-  map_version=1,
-  name="Black Sun Defense - v1.0",
-  norushoffsetX_ARMY_1=0,
-  norushoffsetX_ARMY_2=0,
-  norushoffsetX_ARMY_3=0,
-  norushoffsetX_ARMY_4=0,
-  norushoffsetY_ARMY_1=0,
-  norushoffsetY_ARMY_2=0,
-  norushoffsetY_ARMY_3=0,
-  norushoffsetY_ARMY_4=0,
-  norushradius=80,
-  preview="",
-  save="/maps/BBF_BlackSunDefense/BBF_BlackSunDefense_save.lua",
-  script="/maps/BBF_BlackSunDefense/BBF_BlackSunDefense_script.lua",
-  size={ 1024, 1024 },
-  starts=true,
-  type="skirmish"
+    name = "Black Sun Defense (6 Player) - v2.0",
+    description = "Forked Version to add extra slots (https://github.com/cseufert/fa_blacksunddefence). For Original see http://brunobf.blogspot.com/p/black-sun-defense.html for more information.",
+    preview = '',
+    map_version = 2,
+    type = 'skirmish',
+    starts = true,
+    size = {1024, 1024},
+    reclaim = {0, 0},
+    map = '/maps/BBF_BlackSunDefense.v0002/BBF_BlackSunDefense.scmap',
+    save = '/maps/BBF_BlackSunDefense.v0002/BBF_BlackSunDefense_save.lua',
+    script = '/maps/BBF_BlackSunDefense.v0002/BBF_BlackSunDefense_script.lua',
+    Configurations = {
+        ['standard'] = {
+            teams = {
+                {
+                    name = 'FFA',
+                    armies = {'ARMY_1', 'ARMY_2', 'ARMY_3', 'ARMY_4', 'ARMY_5', 'ARMY_6'}
+                },
+            },
+            customprops={ ExtraArmies="ARMY_HQ ARMY_INCOMING_ENEMY" },
+
+        },
+    },
+    norushoffsetX_ARMY_1=0,
+    norushoffsetX_ARMY_2=0,
+    norushoffsetX_ARMY_3=0,
+    norushoffsetX_ARMY_4=0,
+    norushoffsetX_ARMY_5=0,
+    norushoffsetX_ARMY_6=0,
+    norushoffsetY_ARMY_1=0,
+    norushoffsetY_ARMY_2=0,
+    norushoffsetY_ARMY_3=0,
+    norushoffsetY_ARMY_4=0,
+    norushoffsetY_ARMY_5=0,
+    norushoffsetY_ARMY_6=0,
+    norushradius = 80,
 }
